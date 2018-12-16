7f11674c0524c0053eb060a160d177538edce8b2
Znmangosteen <Znmangosteen@hotmail.com>
2018-12-16 10:39:12 +0800

    after page7 have some problems

--- src/Rental.java
+++ src/Rental.java
@@ -1,23 +1,24 @@
 import java.util.Date;
 
 class Rental {
-    private final Movie movie = new Movie("movie_title", Movie.REGULAR);
+    private Movie _movie;
 
     private int _daysRented;
-    public Rental(String title, int priceCode, Date start, Date end) {
-        movie._title = title;
-        movie._priceCode = priceCode;
-        _daysRented = (int)((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24));
+
+    public Rental(Movie movie, Date start, Date end) {
+        _movie = movie;
+        _daysRented = (int) ((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24));
     }
+
     public int getDaysRented() {
         return _daysRented;
     }
 
     public String getTitle() {
-        return movie._title;
+        return _movie.getTitle();
     }
-    
+
     public int getPriceCode() {
-        return movie._priceCode;
+        return _movie.getPriceCode();
     }
 }
