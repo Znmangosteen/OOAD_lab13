18324e950190e363a4a2e6a0bb0ea0e118926a97
Znmangosteen <Znmangosteen@hotmail.com>
2018-12-18 13:02:22 +0800

    after remove temp

--- src/Customer.java
+++ src/Customer.java
@@ -17,55 +17,42 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
+
 
         for (Rental each : _rentals) {
 
-            //determine amounts for each line
-            double thisAmount = getThisAmount(each);
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getPriceCode() == Movie.NEW_RELEASE)
-                    && each.getDaysRented() > 1) frequentRenterPoints++;
 
             //show figures for this rental
             result.append("\t").append(each.getTitle());
-            result.append("\t").append(String.valueOf(thisAmount));
+            result.append("\t").append(String.valueOf(each.getCharge()));
             result.append("\n");
 
-            totalAmount += thisAmount;
         }
 
         //add footer lines
-        result.append("Amount owed is ").append(String.valueOf(totalAmount));
+        result.append("Amount owed is ").append(String.valueOf(getTotalCharge()));
         result.append("\n");
-        result.append("You earned ").append(String.valueOf(frequentRenterPoints));
+        result.append("You earned ").append(String.valueOf(getTotalFrequentRenterPoints()));
         result.append(" frequent renter points");
         return result.toString();
     }
 
-    private double getThisAmount(Rental aRental) {
-        double thisAmount=0;
-        switch (aRental.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (aRental.getDaysRented() > 2) {
-                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (aRental.getDaysRented() > 3) {
-                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-                }
-                break;
+    public double getTotalCharge() {
+        double result = 0;
+        for (Rental each : _rentals) {
+            result += each.getCharge();
         }
-        return thisAmount;
+        return result;
     }
+
+    public int getTotalFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        for (Rental each : _rentals) {
+            frequentRenterPoints += each.getFrequentRenterPoints();
+        }
+        return frequentRenterPoints;
+    }
+
+
 }
--- src/Rental.java
+++ src/Rental.java
@@ -19,4 +19,37 @@ class Rental {
     public int getPriceCode() {
         return _movie.getPriceCode();
     }
+
+    double getCharge() {
+        double thisAmount=0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2) {
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3) {
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
+
+    public int getFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        //determine amounts for each line
+        // add frequent renter points
+        frequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == Movie.NEW_RELEASE)
+                && getDaysRented() > 1) frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
 }
